d622bbea4e8b1f358716a4010c10c5d8925615ed
kjadhav2021 <kjadhav102015@gmail.com>
2021-04-29 00:30:08 +0800

    added hosts_manager parameterized class
    added hosts_manager parameterized class

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,5 +4,23 @@
 #
 # @example
 #   include hosts_manager
-class hosts_manager {
+class hosts_manager (
+  String $hostname, #'dummy.server11.com'
+  String $ipaddress,#'109.131.4.222'
+){
+  case $facts['kernel'] {
+    'windows': {
+      host { $hostname:
+        ensure => 'present',
+        ip     => $ipaddress,
+        target => 'C:/Windows/System32/drivers/etc/hosts',
+      }
+    }
+    default: {
+        host { $hostname:
+        ensure => 'present',
+        ip     => $ipaddress,
+        }
+    }
+  }
 }
